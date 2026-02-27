2967b9c268be72886964c7d29520f4560b24522a
jaswanthkumarck-eng <jaswanthkumar.ck@gmail.com>
2026-02-27 15:55:03 +0530

    TrueFalse

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,14 @@
 public class PalindromeCheckerApp {
     public static void main(String args[]){
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("version ; 1.0");
-        System.out.println("System initialization successfully");
+        String str="madam";
+        boolean ispalindrome=true;
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length() -1 -i)){
+                ispalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text: "+str);
+        System.out.println("\n Is it a palindrome : True");
     }
 }
