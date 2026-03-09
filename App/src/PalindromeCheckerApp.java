af9fc6c8ff0aa22052f48aa582d35ee225961755
jaswanthkumarck-eng <jaswanthkumar.ck@gmail.com>
2026-03-09 14:45:24 +0530

    UC13

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,23 +1,52 @@
-import java.util.Scanner;
+// UC13: Algorithm comparison implementation
+import java.util.*;
 
 public class PalindromeCheckerApp {
-    public static void main(String args[]){
 
+    public static boolean twoPointerCheck(String input) {
+        int start = 0;
+        int end = input.length() - 1;
 
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string to check if it is a palindrome: ");
-        String originalString = scanner.nextLine();
-        String reversedString = "";
-        for (int i = originalString.length() - 1; i >= 0; i--) {
-            reversedString = reversedString + originalString.charAt(i);
-        }
-        if (originalString.equals(reversedString)) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end))
+                return false;
+            start++;
+            end--;
         }
 
-        scanner.close();
+        return true;
+    }
+
+    public static boolean recursiveCheck(String input, int start, int end) {
+        if (start >= end)
+            return true;
+
+        if (input.charAt(start) != input.charAt(end))
+            return false;
+
+        return recursiveCheck(input, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(input);
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        boolean result2 = recursiveCheck(input, 0, input.length() - 1);
+        long end2 = System.nanoTime();
+
+        System.out.println("Two Pointer Result: " + result1);
+        System.out.println("Execution Time: " + (end1 - start1) + " ns");
+
+        System.out.println("Recursive Result: " + result2);
+        System.out.println("Execution Time: " + (end2 - start2) + " ns");
+
+        sc.close();
     }
 }
